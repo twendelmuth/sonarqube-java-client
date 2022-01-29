77e271da202335b448e0ff33b3cd61bbc508dc9b
thorsten.wendelmuth <thorsten.wendelmuth@gmx.de>
2022-01-29 12:47:40 +0100

    assert that Task parsing works, added all fields, added ZonedDateTime parsing

--- src/test/java/com/twendelmuth/sonarqube/api/ce/ComputeEngineApiTest.java
+++ src/test/java/com/twendelmuth/sonarqube/api/ce/ComputeEngineApiTest.java
@@ -1,7 +1,16 @@
 package com.twendelmuth.sonarqube.api.ce;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +18,7 @@ import com.twendelmuth.sonarqube.api.AbstractApiEndPointTest;
 import com.twendelmuth.sonarqube.api.SonarQubeJsonMapper;
 import com.twendelmuth.sonarqube.api.SonarQubeServer;
 import com.twendelmuth.sonarqube.api.ce.response.ActivityResponse;
+import com.twendelmuth.sonarqube.api.ce.response.Task;
 import com.twendelmuth.sonarqube.api.exception.SonarQubeClientJsonException;
 import com.twendelmuth.sonarqube.api.exception.SonarQubeServerError;
 import com.twendelmuth.sonarqube.api.logging.SonarQubeTestLogger;
@@ -24,10 +34,35 @@ class ComputeEngineApiTest extends AbstractApiEndPointTest<ComputeEngineApi> {
 		ComputeEngineApi computeEngineApi = buildClassUnderTest(getStringFromResource("activity_result.json"));
 
 		ActivityResponse response = computeEngineApi.getActivities(ActivitiesParameter.builder().build());
+		assertEquals(0, getTestLogger().countErrorMessages(), "No error messages expected");
 		assertEquals(2, response.getTasks().size());
 		assertEquals("project_1", response.getTasks().get(0).getComponentKey());
 		assertEquals("project_2", response.getTasks().get(1).getComponentKey());
 		assertEquals(0, getTestLogger().countErrorMessages(), "No error messages expected");
+
+		Task task1 = response.getTasks().get(0);
+		ZoneOffset zoneOffset = ZoneOffset.of("Z");
+		ZoneId zoneId = ZoneId.of("UTC");
+		ZonedDateTime submittedAt = ZonedDateTime.ofInstant(LocalDateTime.of(2015, Month.AUGUST, 13, 21, 34, 59), zoneOffset, zoneId);
+		ZonedDateTime startedAt = ZonedDateTime.ofInstant(LocalDateTime.of(2015, Month.AUGUST, 13, 21, 35, 0), zoneOffset, zoneId);
+		ZonedDateTime executedAt = ZonedDateTime.ofInstant(LocalDateTime.of(2015, Month.AUGUST, 13, 21, 35, 10), zoneOffset, zoneId);
+
+		assertAll(
+				() -> assertEquals("REPORT", task1.getType()),
+				() -> assertEquals("AU-Tpxb--iU5OvuD2FLy", task1.getComponentId()),
+				() -> assertEquals("Project One", task1.getComponentName()),
+				() -> assertEquals("TRK", task1.getComponentQualifier()),
+				() -> assertEquals("AU-TpxcB-iU5Ovu12345", task1.getAnalysisId()),
+				() -> assertEquals("SUCCESS", task1.getStatus()),
+				() -> assertEquals("john", task1.getSubmitterLogin()),
+				() -> assertEquals(10000L, task1.getExecutionTimeMs()),
+				() -> assertFalse(task1.isLogs()),
+				() -> assertFalse(task1.isHasErrorStacktrace()),
+				() -> assertTrue(task1.isHasScannerContext()),
+				() -> assertEquals(submittedAt, task1.getSubmittedAt()),
+				() -> assertEquals(startedAt, task1.getStartedAt()),
+				() -> assertEquals(executedAt, task1.getExecutedAt()));
+
 	}
 
 	@Test
@@ -35,10 +70,10 @@ class ComputeEngineApiTest extends AbstractApiEndPointTest<ComputeEngineApi> {
 		ComputeEngineApi computeEngineApi = buildClassUnderTest(getStringFromResource("activity_result.json"));
 
 		ActivityResponse response = computeEngineApi.getActivities(null);
+		assertEquals(0, getTestLogger().countErrorMessages(), "No error messages expected");
 		assertEquals(2, response.getTasks().size());
 		assertEquals("project_1", response.getTasks().get(0).getComponentKey());
 		assertEquals("project_2", response.getTasks().get(1).getComponentKey());
-		assertEquals(0, getTestLogger().countErrorMessages(), "No error messages expected");
 	}
 
 	@Test
