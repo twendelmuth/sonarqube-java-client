37a50dda67a50e766f3115d06f6f1b2a602cbc01
thorsten.wendelmuth <thorsten.wendelmuth@gmx.de>
2022-01-30 17:07:35 +0100

    reduced number of versions

--- src/test/java/com/twendelmuth/sonarqube/api/it/docker/SonarQubeVersion.java
+++ src/test/java/com/twendelmuth/sonarqube/api/it/docker/SonarQubeVersion.java
@@ -7,10 +7,10 @@ import java.util.stream.Collectors;
 import com.twendelmuth.sonarqube.api.SonarQubeLicense;
 
 public enum SonarQubeVersion {
-	V8_2("8.2-community", SonarQubeLicense.COMMUNITY),
-	V8_9_LATEST("8.9-community", SonarQubeLicense.COMMUNITY),
-	V8_9_LTS_DEVELOPER("8.9-developer", SonarQubeLicense.DEVELOPER),
-	V8_9_LTS_ENTERPRISE("8.9-enterprise", SonarQubeLicense.ENTERPRISE),
+	//	V8_2("8.2-community", SonarQubeLicense.COMMUNITY),
+	//	V8_9_LATEST("8.9-community", SonarQubeLicense.COMMUNITY),
+	//	V8_9_LTS_DEVELOPER("8.9-developer", SonarQubeLicense.DEVELOPER),
+	//	V8_9_LTS_ENTERPRISE("8.9-enterprise", SonarQubeLicense.ENTERPRISE),
 	V9_LATEST("9-community", SonarQubeLicense.COMMUNITY),
 	V9_LATEST_DEVELOPER("9-developer", SonarQubeLicense.DEVELOPER),
 	V9_LATEST_ENTERPRISE("9-enterprise", SonarQubeLicense.ENTERPRISE);
